df47720d8ee7b4f0f8005bb0df7749ad1a6f6943
pvburger <iniidabyss@gmail.com>
2025-11-13 08:03:34 -0500

    Implemented seeking functionality for video files

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { QueryRequest } from '../types';
+import { QueryRequest, VideoStats } from '../types';
 import fs from 'fs';
 import path from 'path';
 import crypto from 'crypto';
@@ -8,12 +8,37 @@ import crypto from 'crypto';
 export default function createServer() {
   const server = express();
 
+  // hardcoded port
   const PORT = 3333;
 
-  // function to check if 'req.query.path' is valid type
-  const reqIsValid = (inp: QueryRequest): boolean => {
-    // any type other than a simple string is invalid in this case
-    return typeof inp === 'string';
+  // function to return object with video stats based on request header string
+  const getVidStats = (inpPath: string, inpRange: string): VideoStats => {
+    try {
+      const result = new VideoStats();
+
+      // this could be implemented asynchronously
+      const vStats = fs.statSync(inpPath);
+
+      const startIdx = inpRange.indexOf('=') + 1;
+      const endIdx = inpRange.indexOf('-') + 1;
+      result.duration = vStats.size;
+
+      if (endIdx === startIdx + 1) {
+        // using a negative index; eg, bytes=-500
+        result.start = result.duration - parseInt(inpRange.slice(endIdx), 10);
+
+        // should be a normal end with start and end; eg, bytes=0-500
+      } else {
+        result.start = parseInt(inpRange.slice(startIdx, endIdx), 10);
+      }
+
+      result.end = vStats.size - 1;
+
+      return result;
+    } catch (err) {
+      console.log(`There was a problem retrieving video information: ${err}`);
+      return new VideoStats();
+    }
   };
 
   // server.use(cors());
@@ -24,8 +49,10 @@ export default function createServer() {
   // router for all valid endpoints
   server.get('/video', (req, res) => {
     const filePath = req.query.path;
-    // no path parameter
-    if (!reqIsValid(filePath)) {
+    const range = req.headers.range;
+
+    // type check
+    if (typeof filePath !== 'string' || !range) {
       return res.status(400).send('Invalid path');
     }
 
@@ -38,8 +65,35 @@ export default function createServer() {
       return res.status(404).send('File not found');
     }
 
-    res.writeHead(200, { 'Content-Type': 'video/mp4' });
-    fs.createReadStream(filePath as string).pipe(res);
+    // added for development
+    console.log(`Range value: ${req.headers.range}`);
+
+    // added for development
+    console.log('Range specified');
+    const vidStats = getVidStats(filePath as string, range);
+    if (
+      vidStats.start === null ||
+      vidStats.end === null ||
+      vidStats.duration === null
+    ) {
+      // added for development
+      console.log(`Invalid range...`);
+      res.writeHead(200, { 'Content-Type': 'video/mp4' });
+      return fs.createReadStream(filePath as string).pipe(res);
+    } else {
+      res.writeHead(206, {
+        'Content-Range': `bytes ${vidStats.start}-${vidStats.end}/${vidStats.duration}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': vidStats.end - vidStats.start + 1,
+        'Content-Type': 'video/mp4',
+      });
+      return fs
+        .createReadStream(filePath as string, {
+          start: vidStats.start,
+          end: vidStats.end,
+        })
+        .pipe(res);
+    }
   });
 
   // catch-all error handler
--- types.ts
+++ types.ts
@@ -1,9 +1,23 @@
 import { ParsedQs } from 'qs';
 
-
 export type ButtonProp = {
   label: string;
   runFun: () => void;
 };
 
-export type QueryRequest = (string | ParsedQs) | (string | ParsedQs)[] | undefined;
+export type QueryRequest =
+  | (string | ParsedQs)
+  | (string | ParsedQs)[]
+  | undefined;
+
+export class VideoStats {
+  start: number | null;
+  end: number | null;
+  duration: number | null;
+
+  constructor() {
+    this.start = null;
+    this.end = null;
+    this.duration = null;
+  }
+}
